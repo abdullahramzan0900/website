1b6d0fbdef20ecc929c56e699ade7c6ee7558525
abdullahramzan0900 <abdullahramzan0900@gmail.com>
2023-01-19 14:50:54 -0600

    i am here

--- src/ABOUT/Message.jsx
+++ src/ABOUT/Message.jsx
@@ -6,55 +6,11 @@ function Message()
 {
     return(
         <>
-           <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 2, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Name"
-         
         
-        />
-           <TextField
-          required
-          id="outlined-required"
-          label="Last name"
-         
-        
-        />
-                
     
       
 
-      </div>
-      <div>
-                
-    
-           <TextField
-          required
-          id="outlined-required"
-          label="Last name"
-         
-        
-        />
-      </div>
-      <TextField
-    sx={{
-        width: 700
-    }}
-    InputProps={{ sx: { height: 80 } }}
-    placeholder="SX + InputProps"
-/>
-    
-    
-    </Box> 
+
         
         </>
     )
